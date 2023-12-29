cf7d9610564df27aaf6a380cfeddcc93cd277c34
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2023-12-29 13:32:15 +0500

    updated host.ts

--- const/host.ts
+++ const/host.ts
@@ -1,4 +1,5 @@
-export const api_host: string = 'https://www.api.v2.darkmaterial.space'
-// process.env.NODE_ENV === 'development' 
-// ? 'http://localhost:8000' 
-// : 'https://www.api.v2.darkmaterial.space'
+export const api_host: string = 
+// 'https://www.api.v2.darkmaterial.space'
+process.env.NODE_ENV === 'development' 
+? process.env.NEXT_PUBLIC_API_HOST_DEV as string
+: process.env.NEXT_PUBLIC_API_HOST_PROD as string
