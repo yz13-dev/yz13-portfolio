df24e9c93a8468d78f10005a4df3df99c5c66853
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2024-03-10 10:43:20 +0500

    updated deps, and added web-vitals

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from 'next'
 import { Geologica } from 'next/font/google'
+import { WebVitals } from "ui"
 import 'ui/dist/style.css'
 import './globals.css'
 const first_font = Geologica({ subsets: ['latin', 'cyrillic'], weight: ['600', '500', '400', '300', '200'], variable: '--root-font' })
@@ -16,6 +17,7 @@ export default function RootLayout({ children }: { children: JSX.Element }) {
   return (
     <html lang="en" className={`${first_font.className} ${first_font.variable}`}>
       <body className='min-h-screen dark'>
+        <WebVitals appId="darkmaterial-yz13" />
         {children}
       </body>
     </html>
