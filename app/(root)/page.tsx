222bb6f6cd9215daa4d681e5f87ca747dfe0c1cc
yz13-dev <yztheceo@yandex.ru>
2025-07-08 16:08:59 +0500

    small design update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Background />
-      <main className="flex flex-col h-[calc(100dvh-78px)] max-w-md mx-auto *:max-w-sm px-8 pt-8 gap-[32px] justify-center items-center sm:items-start">
+      <main className="flex flex-col h-dvh max-w-md mx-auto *:max-w-sm p-8 gap-[32px] justify-center items-center sm:items-start">
         <div className="space-y-4">
           <div className="flex items-center gap-2">
             <Logo size={48} type="icon" />
@@ -43,24 +43,26 @@ export default function Home() {
           </section>
         </div>
 
-        <div className="w-full max-w-xs">
-          <span className="text-muted-foreground text-center text-xs">
-            По вопросам и/или предложениям пишите:
-          </span>
-          <div className="flex items-center gap-2 text-xs">
-            <Link href="mailto:yz13.dev@gmail.com" className="font-medium text-foreground hover:underline">yz13.dev@gmail.com</Link>
-            <span className="text-muted-foreground">или</span>
-            <Link href="mailto:yztheceo@yandex.ru" className="font-medium text-foreground hover:underline">yztheceo@yandex.ru</Link>
+        <div className="w-full space-y-4 sm:mt-0 mt-auto">
+          <div className="w-full max-w-xs">
+            <Availability className="bg-transparent !px-0 !py-0 border-0" size="sm" />
+            <div className="w-full">
+              <span className="text-muted-foreground text-center text-xs">
+                По вопросам и/или предложениям пишите:
+              </span>
+              <div className="flex items-center gap-1.5 text-xs">
+                <Link href="mailto:yz13.dev@gmail.com" className="font-medium text-foreground hover:underline">yz13.dev@gmail.com</Link>
+                <span className="text-muted-foreground">или</span>
+                <Link href="mailto:yztheceo@yandex.ru" className="font-medium text-foreground hover:underline">yztheceo@yandex.ru</Link>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex gap-4 items-center flex-col sm:*:w-fit *:w-full sm:flex-row w-full">
+            <CallToAction />
           </div>
         </div>
-
-        <div className="flex gap-4 sm:mt-0 mt-auto items-center flex-col sm:*:w-fit *:w-full sm:flex-row w-full">
-          <CallToAction />
-        </div>
       </main>
-      <footer className="absolute bottom-0 p-6 left-0 right-0 mx-auto flex flex-col items-center gap-6 max-w-md">
-        <Availability className="bg-background/40" />
-      </footer>
     </>
   );
 }
