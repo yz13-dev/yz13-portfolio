652fdc0e1e5d7e5181d8c6bd3a46127d788f3581
yz13-dev <yztheceo@yandex.ru>
2025-07-10 20:02:16 +0500

    fix favicon link in root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -20,6 +20,11 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
   },
+  {
+    rel: "icon",
+    href: "/favicon.ico",
+    type: "image/x-icon",
+  },
 ];
 
 export const meta: MetaFunction = () => {
