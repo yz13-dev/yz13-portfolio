601a3c22607eb08cd3abc38a17468948ce401500
yz13-dev <yztheceo@yandex.ru>
2025-07-13 15:04:02 +0500

    replace local flag lib to @yz13/flags

--- app/flags/flags.ts
+++ app/flags/flags.ts
@@ -1,51 +1,11 @@
-
-type FlagsSchema = {
-  version: string,
-  updated_at: string
-  items: Record<string, any>
-}
-
-
-const client = async<T>({ baseUrl, path }: { baseUrl: string, path: string }): Promise<T> => {
-  const url = new URL(path, baseUrl)
-
-  const response = await fetch(url.toString())
-  if (response.status !== 200) {
-    throw new Error(`Failed to fetch flags`)
-  }
-  return response.json()
-}
-
-const bucketClient = client<FlagsSchema>({ baseUrl: "https://cdn.yz13.ru", path: "flags.json" })
-
-const flag = async <T>(key: string, defaultValue: T): Promise<T> => {
-  try {
-    const flags = await bucketClient
-    const value = flags.items[key]
-
-    if (value === undefined || value === null) {
-      if (defaultValue !== undefined) {
-        return defaultValue
-      }
-      // Возвращаем автоматически определенное значение по умолчанию
-      return defaultValue;
-    }
-
-    return value as T
-  } catch (error) {
-    if (defaultValue !== undefined) {
-      return defaultValue
-    }
-    // Возвращаем автоматически определенное значение по умолчанию при ошибке
-    return defaultValue
-  }
-}
+import { createClient } from "@yz13/flags"
+const client = createClient({ appId: "yz13" })
 
 // Теперь можно использовать без явного указания defaultValue
 export const availableForWork = async (): Promise<boolean> => {
-  return flag<boolean>("available-for-work", false) // Автоматически вернет false
+  return await client.get<boolean>("available-for-work") ?? false // Автоматически вернет false
 }
 
 export const getInfoList = async (): Promise<string[]> => {
-  return flag<string[]>("info-list", []) // Автоматически вернет []
+  return await client.get<string[]>("info-list") ?? [] // Автоматически вернет []
 }
