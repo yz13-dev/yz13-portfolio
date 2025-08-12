5b0a29824f3d6c39dbd9baa52e67000979f3e37c
yz13-dev <yztheceo@yandex.ru>
2025-08-12 20:30:38 +0500

    change root layout

--- app/routes/(root)/page.tsx
+++ app/routes/(root)/page.tsx
@@ -80,9 +80,9 @@ export function ErrorBoundary() {
   const errorDescription = error instanceof Error ? error.stack : isRouteErrorResponse(error) ? error.data : undefined;
 
   return (
-    <div className="w-full h-dvh flex md:flex-row flex-col overflow-y-auto">
+    <>
       <DitheringBackground className="opacity-15" />
-      <div className="md:w-1/2 w-full md:min-h-fit min-h-dvh md:h-full h-fit flex flex-col justify-between *:p-6 md:sticky static top-0">
+      <div className="w-full md:min-h-fit h-dvh flex flex-col justify-between *:p-6">
         <header className="w-full space-y-6">
           <Logo size={28} type="full" />
           <div className="flex flex-col gap-3">
@@ -114,7 +114,7 @@ export function ErrorBoundary() {
           </div>
         </div>
       </div>
-      <div className="md:w-1/2 w-full md:h-full h-fit *:p-6">
+      <div className="w-full max-w-7xl mt-[5%] mx-auto">
         <div className="w-full">
           <span className="text-foreground md:text-4xl text-3xl font-semibold">{errorTitle}</span>
         </div>
@@ -124,15 +124,15 @@ export function ErrorBoundary() {
           </span>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
 export function HydrateFallback() {
   return (
-    <div className="w-full h-dvh flex md:flex-row flex-col overflow-y-auto">
+    <>
       <DitheringBackground className="opacity-15" />
-      <div className="md:w-1/2 w-full md:min-h-fit min-h-dvh md:h-full h-fit flex flex-col justify-between *:p-6 md:sticky static top-0">
+      <div className="w-full md:min-h-fit h-dvh flex flex-col justify-between *:p-6">
         <header className="w-full space-y-6">
           <Logo size={28} type="full" />
           <div className="flex flex-col gap-3">
@@ -184,7 +184,7 @@ export function HydrateFallback() {
           }
         </div>
       </div>
-      <div className="md:w-1/2 w-full md:h-full h-fit *:p-6">
+      <div className="w-full max-w-7xl mt-[5%] mx-auto">
 
         <div className="space-y-6">
           <div className="w-full">
@@ -210,7 +210,7 @@ export function HydrateFallback() {
         </div>
 
       </div>
-    </div>
+    </>
   )
 }
 
@@ -218,9 +218,9 @@ export default function () {
   const { publications, available, pricing } = useLoaderData<typeof loader>();
 
   return (
-    <div className="w-full h-dvh flex md:flex-row flex-col overflow-y-auto">
+    <>
       <DitheringBackground className="opacity-15" />
-      <div className="md:w-1/2 w-full md:min-h-fit min-h-dvh md:h-full h-fit flex flex-col justify-between *:p-6 md:sticky static top-0">
+      <div className="w-full md:min-h-fit h-dvh flex flex-col justify-between *:p-6">
         <header className="w-full space-y-6">
           <Logo size={28} type="full" />
           <div className="flex flex-col gap-3">
@@ -237,7 +237,7 @@ export default function () {
               {(available) => <Availability size="lg" enabled={available} />}
             </Await>
           </Suspense>
-          <main className="w-full space-y-3 *:block">
+          <main className="w-full space-y-3 max-w-3xl *:block">
             <h1 className="text-foreground xl:text-6xl lg:text-5xl md:text-4xl text-3xl font-semibold">{title}</h1>
             <p className="text-muted-foreground xl:text-4xl lg:text-3xl md:text-2xl text-xl font-medium">
               {description}
@@ -284,184 +284,182 @@ export default function () {
           }
         </div>
       </div>
-      <div className="md:w-1/2 w-full h-fit lg:px-6 px-0 lg:pb-6 pb-0 lg:pt-[70dvh] pt-6">
-        <div className="w-full bg-card/10 backdrop-blur-sm rounded-t-4xl lg:rounded-b-4xl rounded-n-none border">
-          <Section>
-            <div className="w-full p-6">
-              <SectionTitle >Последние работы</SectionTitle>
-            </div>
-            <Suspense fallback={<RecentProjectsSkeleton />}>
-              <Await
-                resolve={publications}
-                errorElement={<RecentProjectsError />}
-              >
-                {
-                  (projects) =>
-                    <RecentProjects projects={projects} />
+      <div className="w-full max-w-7xl mt-[5%] mx-auto">
+        <Section>
+          <div className="w-full p-6">
+            <SectionTitle >Последние работы</SectionTitle>
+          </div>
+          <Suspense fallback={<RecentProjectsSkeleton />}>
+            <Await
+              resolve={publications}
+              errorElement={<RecentProjectsError />}
+            >
+              {
+                (projects) =>
+                  <RecentProjects projects={projects} />
+              }
+            </Await>
+          </Suspense>
+        </Section>
+        <Section className="space-y-6 p-6">
+          <SectionContent>
+            <GithubContributions username="yz13-dev" />
+          </SectionContent>
+        </Section>
+        <div className="w-full divide-y">
+          <Suspense fallback={
+            <>
+              <Section className="space-y-6 p-6">
+                <div className="w-full">
+                  <SectionTitle >Услуги и цены</SectionTitle>
+                </div>
+                <PricingDurationSkeleton />
+              </Section>
+              <Section className="space-y-6 p-6">
+                <PricingDetailsSkeleton />
+              </Section>
+            </>
+          }>
+            <Await
+              resolve={pricing}
+            >
+              {
+                (pricing) => {
+                  const sorted = pricing.sort((a, b) => {
+                    const aDuration = a.duration.reduce((prev, current) => prev + current, 0)
+                    const bDuration = b.duration.reduce((prev, current) => prev + current, 0)
+                    const aPricing = a.price;
+                    const bPricing = b.price;
+                    if (aDuration === bDuration) return aPricing - bPricing;
+                    return aDuration - bDuration
+                  })
+                  return (
+                    <>
+                      <Section className="space-y-6 p-6">
+                        <div className="w-full">
+                          <SectionTitle >Услуги и цены</SectionTitle>
+                        </div>
+                        <PricingDuration pricing={sorted} />
+                      </Section>
+                      <Section className="space-y-6 p-6">
+                        <PricingDetails pricing={sorted} />
+                      </Section>
+                    </>
+                  )
                 }
-              </Await>
-            </Suspense>
-          </Section>
+              }
+            </Await>
+          </Suspense>
           <Section className="space-y-6 p-6">
+            <div className="w-full">
+              <SectionTitle >Вопросы и ответы</SectionTitle>
+            </div>
             <SectionContent>
-              <GithubContributions username="yz13-dev" />
+              <Accordion
+                type="multiple"
+                className="rounded-lg bg-card border"
+              >
+                <AccordionItem value="q-1" className="*:px-5 *:text-base">
+                  <AccordionTrigger className="text-base data-[state=open]:text-muted-foreground">
+                    Как быстро начнется разработка?
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    После определения задач разработки (1-2 созвона), работа обычно начнется на следующий день. Кроме выходных дней.
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="q-2" className="*:px-5 *:text-base">
+                  <AccordionTrigger className="text-base data-[state=open]:text-muted-foreground">
+                    Есть ли лимит к поправкам?
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    К небольшим поправкам - нет. К большим поправкам - да. В ценниках указана сумма за большие поправки. Небольшие идут бесплатно.
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="q-3" className="*:px-5 *:text-base">
+                  <AccordionTrigger className="text-base data-[state=open]:text-muted-foreground">
+                    Что я получу в конце разработки?
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    В конце разработки вы получаете свой проект на GitHub и получаете доступ к базе данных и функционалу сайта.
+                    Если вы заказали NPM-пакет, то вы получите доступ к нему и сможете установить его в своем проекте. При необходимости можно запросить архив вместо github репозитория.
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
             </SectionContent>
           </Section>
-          <div className="w-full divide-y">
-            <Suspense fallback={
-              <>
-                <Section className="space-y-6 p-6">
-                  <div className="w-full">
-                    <SectionTitle >Услуги и цены</SectionTitle>
-                  </div>
-                  <PricingDurationSkeleton />
-                </Section>
-                <Section className="space-y-6 p-6">
-                  <PricingDetailsSkeleton />
-                </Section>
-              </>
-            }>
-              <Await
-                resolve={pricing}
-              >
-                {
-                  (pricing) => {
-                    const sorted = pricing.sort((a, b) => {
-                      const aDuration = a.duration.reduce((prev, current) => prev + current, 0)
-                      const bDuration = b.duration.reduce((prev, current) => prev + current, 0)
-                      const aPricing = a.price;
-                      const bPricing = b.price;
-                      if (aDuration === bDuration) return aPricing - bPricing;
-                      return aDuration - bDuration
-                    })
-                    return (
-                      <>
-                        <Section className="space-y-6 p-6">
-                          <div className="w-full">
-                            <SectionTitle >Услуги и цены</SectionTitle>
-                          </div>
-                          <PricingDuration pricing={sorted} />
-                        </Section>
-                        <Section className="space-y-6 p-6">
-                          <PricingDetails pricing={sorted} />
-                        </Section>
-                      </>
-                    )
-                  }
-                }
-              </Await>
-            </Suspense>
-            <Section className="space-y-6 p-6">
-              <div className="w-full">
-                <SectionTitle >Вопросы и ответы</SectionTitle>
-              </div>
-              <SectionContent>
-                <Accordion
-                  type="multiple"
-                  className="rounded-lg bg-card border"
-                >
-                  <AccordionItem value="q-1" className="*:px-5 *:text-base">
-                    <AccordionTrigger className="text-base data-[state=open]:text-muted-foreground">
-                      Как быстро начнется разработка?
-                    </AccordionTrigger>
-                    <AccordionContent>
-                      После определения задач разработки (1-2 созвона), работа обычно начнется на следующий день. Кроме выходных дней.
-                    </AccordionContent>
-                  </AccordionItem>
-                  <AccordionItem value="q-2" className="*:px-5 *:text-base">
-                    <AccordionTrigger className="text-base data-[state=open]:text-muted-foreground">
-                      Есть ли лимит к поправкам?
-                    </AccordionTrigger>
-                    <AccordionContent>
-                      К небольшим поправкам - нет. К большим поправкам - да. В ценниках указана сумма за большие поправки. Небольшие идут бесплатно.
-                    </AccordionContent>
-                  </AccordionItem>
-                  <AccordionItem value="q-3" className="*:px-5 *:text-base">
-                    <AccordionTrigger className="text-base data-[state=open]:text-muted-foreground">
-                      Что я получу в конце разработки?
-                    </AccordionTrigger>
-                    <AccordionContent>
-                      В конце разработки вы получаете свой проект на GitHub и получаете доступ к базе данных и функционалу сайта.
-                      Если вы заказали NPM-пакет, то вы получите доступ к нему и сможете установить его в своем проекте. При необходимости можно запросить архив вместо github репозитория.
-                    </AccordionContent>
-                  </AccordionItem>
-                </Accordion>
-              </SectionContent>
-            </Section>
-          </div>
-          <div className="w-full">
-            <footer className={cn(
-              "flex 2xl:flex-row flex-col-reverse w-full h-git gap-6",
-              "lg:*:w-1/2 *:w-full *:gap-6 py-6 px-6 border-y"
-            )}>
-              <div className="flex flex-col">
-                <div className="w-full flex flex-col gap-3">
-                  <Logo size={48} />
-                  <div className="*:block space-y-1">
-                    <span className="text-base text-foreground font-semibold">{title}</span>
-                    <span className="text-sm text-muted-foreground">{description}</span>
-                  </div>
-                </div>
-                <div className="w-full flex flex-col *:w-full gap-3 *:h-10">
-                  <Suspense fallback={<Skeleton className="h-9 w-32" />}>
-                    <Await resolve={available}>
-                      {(available) => <Button disabled={!available}>
-                        <span className="sm:inline hidden">Запланировать видеозвонок</span>
-                        <span className="sm:hidden inline">Видеозвонок</span>
-                        <ArrowRightIcon />
-                      </Button>}
-                    </Await>
-                  </Suspense>
-                  <Suspense fallback={<AvailabilitySkeleton />}>
-                    <Await resolve={available}>
-                      {(available) => <Availability size="default" animated={false} enabled={available} className="justify-center" />}
-                    </Await>
-                  </Suspense>
+        </div>
+        <div className="w-full">
+          <footer className={cn(
+            "flex 2xl:flex-row flex-col-reverse w-full h-git gap-6",
+            "lg:*:w-1/2 *:w-full *:gap-6 py-6 px-6 border-y"
+          )}>
+            <div className="flex flex-col">
+              <div className="w-full flex flex-col gap-3">
+                <Logo size={48} />
+                <div className="*:block space-y-1">
+                  <span className="text-base text-foreground font-semibold">{title}</span>
+                  <span className="text-sm text-muted-foreground">{description}</span>
                 </div>
               </div>
-              <Separator className="2xl:hidden block" />
-              <div className="w-full h-fit flex sm:flex-row flex-col md:*:w-1/2 *:w-full">
-                <div className="w-1/3 flex flex-col gap-3">
-                  <span>Ссылки</span>
-                  <ul className="*:text-muted-foreground">
-                    <li className="flex items-center gap-2 h-9">
-                      <Link
-                        to={telegram}
-                        className="text-sm inline-flex items-center gap-1.5 hover:underline hover:text-foreground"
-                      >
-                        Telegram
-                        <ExternalLinkIcon size={12} />
-                      </Link>
-                    </li>
-                    <li className="flex items-center gap-2 h-9">
-                      <Link
-                        to={twitter}
-                        className="text-sm inline-flex items-center gap-1.5 hover:underline hover:text-foreground"
-                      >
-                        X/Twitter
-                        <ExternalLinkIcon size={12} />
-                      </Link>
-                    </li>
-                    <li className="flex items-center gap-2 h-9">
-                      <Link
-                        to={github}
-                        className="text-sm inline-flex items-center gap-1.5 hover:underline hover:text-foreground"
-                      >
-                        Github
-                        <ExternalLinkIcon size={12} />
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
+              <div className="w-full flex flex-col *:w-full gap-3 *:h-10">
+                <Suspense fallback={<Skeleton className="h-9 w-32" />}>
+                  <Await resolve={available}>
+                    {(available) => <Button disabled={!available}>
+                      <span className="sm:inline hidden">Запланировать видеозвонок</span>
+                      <span className="sm:hidden inline">Видеозвонок</span>
+                      <ArrowRightIcon />
+                    </Button>}
+                  </Await>
+                </Suspense>
+                <Suspense fallback={<AvailabilitySkeleton />}>
+                  <Await resolve={available}>
+                    {(available) => <Availability size="default" animated={false} enabled={available} className="justify-center" />}
+                  </Await>
+                </Suspense>
               </div>
-            </footer>
-          </div>
-          <div className="flex p-6 items-center justify-between">
-            <span className="text-xs text-muted-foreground">YZ13 2025</span>
-            <span className="text-xs text-muted-foreground">Фронтенд разработчик</span>
-          </div>
+            </div>
+            <Separator className="2xl:hidden block" />
+            <div className="w-full h-fit flex sm:flex-row flex-col md:*:w-1/2 *:w-full">
+              <div className="w-1/3 flex flex-col gap-3">
+                <span>Ссылки</span>
+                <ul className="*:text-muted-foreground">
+                  <li className="flex items-center gap-2 h-9">
+                    <Link
+                      to={telegram}
+                      className="text-sm inline-flex items-center gap-1.5 hover:underline hover:text-foreground"
+                    >
+                      Telegram
+                      <ExternalLinkIcon size={12} />
+                    </Link>
+                  </li>
+                  <li className="flex items-center gap-2 h-9">
+                    <Link
+                      to={twitter}
+                      className="text-sm inline-flex items-center gap-1.5 hover:underline hover:text-foreground"
+                    >
+                      X/Twitter
+                      <ExternalLinkIcon size={12} />
+                    </Link>
+                  </li>
+                  <li className="flex items-center gap-2 h-9">
+                    <Link
+                      to={github}
+                      className="text-sm inline-flex items-center gap-1.5 hover:underline hover:text-foreground"
+                    >
+                      Github
+                      <ExternalLinkIcon size={12} />
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </footer>
+        </div>
+        <div className="flex p-6 items-center justify-between">
+          <span className="text-xs text-muted-foreground">YZ13 2025</span>
+          <span className="text-xs text-muted-foreground">Фронтенд разработчик</span>
         </div>
       </div>
-    </div>
+    </>
   )
 }
