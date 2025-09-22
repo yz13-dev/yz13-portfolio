160d4bbad90f477570ab7f10db85ff4ed22a68b1
yz13-dev <yztheceo@yandex.ru>
2025-09-22 17:06:41 +0500

    continue redesigning

--- app/routes/page.tsx
+++ app/routes/page.tsx
@@ -1,4 +1,5 @@
 import { Logo } from "@/components/logo";
+import User from "@/components/user";
 import { available } from "@/utils/flags";
 import { Button } from "@yz13/ui/button";
 import { ArrowRightIcon, SendIcon } from "lucide-react";
@@ -30,7 +31,18 @@ export default function () {
 
   return (
     <>
-      <main className="w-full h-dvh"></main>
+      <header className="w-full max-w-7xl flex items-center justify-between mx-auto py-3 px-6">
+        <Logo size={48} type="full" />
+        <User />
+      </header>
+      <main className="w-full max-w-7xl mx-auto px-6 py-[5%] space-y-6">
+        <div className="size-24 rounded-lg bg-card border flex items-center justify-center">
+          <Logo size={64} type="icon" />
+        </div>
+        <h1 className="*:inline space-x-2 *:text-4xl *:font-semibold max-w-2xl">
+          <span>YZ13</span><span>-</span><span>Фронтенд который не подведет. Разработаю сайт, страницы, приложение и компоненты разной сложности</span>
+        </h1>
+      </main>
       <footer className="w-full max-w-7xl mx-auto p-6 space-y-6">
         <div className="w-full flex md:flex-row flex-col gap-5 md:*:w-1/3 *:w-full">
           <div className="w-fit space-y-1.5">
