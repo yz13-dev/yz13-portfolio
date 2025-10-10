9b0badb49059fe2877366abf033f8199c947ed2d
yz13-dev <yztheceo@yandex.ru>
2025-10-10 10:20:46 +0500

    add drawer for mobiles instead of dialog

--- app/hooks/use-mobile.ts
+++ app/hooks/use-mobile.ts
@@ -1,18 +1,15 @@
 import { configResponsive, useResponsive } from "ahooks";
 
-
 configResponsive({
   small: 576,
   middle: 1024,
   large: 1600,
 });
 
-
 export const useMobile = () => {
-
   const responsive = useResponsive();
 
-  const small = !responsive["middle"];
+  const small = !responsive?.["middle"] || false;
 
   return small;
-}
+};
