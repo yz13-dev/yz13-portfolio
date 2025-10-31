fbbd64288085d11a874653bddbf3ef9a85e01477
yz13-dev <yztheceo@yandex.ru>
2025-10-31 20:05:28 +0500

    update cdn.ts

--- app/utils/cdn.ts
+++ app/utils/cdn.ts
@@ -1,2 +1,5 @@
+
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL;
+
 export const cdn = (path: string) =>
-  `${import.meta.env.VITE_SUPABASE_URL}/storage/v1/object/public${path}`;
+  `${SUPABASE_URL}/storage/v1/object/public${path}`;
